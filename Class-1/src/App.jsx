a06aec0320992189152adee770463027ff9cf197
Muhammad-Shayan001 <shayan.javed091@gmail.com>
2025-10-15 12:18:42 +0500

    This is my 1st class of React

---
+++ Class-1/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className="head">Hello world</h1>
+      <p className="para">
+        Welcome to React 18 with Vite!
+        <br/>
+        This is my first class of React
+      </p>
+    </>
+  );
+}
+
+export default App;
